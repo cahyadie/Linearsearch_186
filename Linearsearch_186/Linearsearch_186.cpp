80704bf7a9ee695b4a5bf2470dfebf97837a84e6
cahyadie <cahyadiilham12@gmail.com>
2023-04-13 14:43:00 +0700

    void input dan membuat [erulangan

--- Linearsearch_186/Linearsearch_186.cpp
+++ Linearsearch_186/Linearsearch_186.cpp
@@ -5,3 +5,24 @@ int arr[20];
 int n;
 int i;
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+	cout << "\n------------------\n";
+	cout << "Enter array elements\n";
+	cout << "--------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
